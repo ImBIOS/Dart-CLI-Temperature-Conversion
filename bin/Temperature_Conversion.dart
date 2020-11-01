3d90ac4f9db3c50e543c8032cdda922b2debd207
Imamuzzaki Abu Salam <41441643+ImBIOS@users.noreply.github.com>
2020-11-01 09:58:36 +0700

    indonesian into english and do documentation

--- bin/Temperature_Conversion.dart
+++ bin/Temperature_Conversion.dart
@@ -1,28 +1,41 @@
+//import dart input output library
 import 'dart:io';
 
+//start void main script
 void main() {
-  var suhuAwal = ['Fahrenheit', 'Celsius', 'Reamur', 'Kelvin'];
-  print('Pilih jenis suhu asal (Angka):');
+  //declare array containing 4 different name of temperature name
+  var tName = ['Fahrenheit', 'Celcius', 'Reamur', 'Kelvin'];
+  
+  //print into terminal prompt for user to input the temperature value
+  print('Choose type of temperature to convert (in number):');
+  
+  //logic to print list of the tName in separate line
   for (var i = 1; i <= 4; i++) {
     stdout.write('$i. ');
-    stdout.write('${suhuAwal[i - 1]}\n');
+    stdout.write('${tName[i - 1]}\n');
   }
-  var jenisAsal = num.parse(stdin.readLineSync());
-  stdout.write('Nilai suhu asal: ');
-  var nilaiAsal = num.parse(stdin.readLineSync());
-  print('Pilih jenis suhu hasil (Angka):');
+  
+  //choose the number from the list of temperature type
+  var typeBefore = num.parse(stdin.readLineSync());
+  stdout.write('Temperature value to convert: ');
+  
+  //choose the ype of temperature for the result of conversion
+  var valueBefore = num.parse(stdin.readLineSync());
+  print('Choose type of converted temperature (in number):');
+  
+  //List the item
   for (var i = 1; i <= 4; i++) {
     stdout.write('$i. ');
-    stdout.write('${suhuAwal[i - 1]}\n');
+    stdout.write('${tName[i - 1]}\n');
   }
-  var jenisHasil = num.parse(stdin.readLineSync());
+  var typeResult = num.parse(stdin.readLineSync());
   var fahrenheit, reamur, celsius, kelvin;
-  if (jenisAsal == 4) {
-    kelvin = nilaiAsal;
+  if (typeBefore == 4) {
+    kelvin = valueBefore;
     celsius = kelvin - 273;
     fahrenheit = celsius * 9 / 5 + 32;
     reamur = celsius * 4 / 5;
   }
   var suhuHasil = [fahrenheit, celsius, reamur, kelvin];
-  print('$nilaiAsal° = ${suhuHasil[jenisHasil-1].toStringAsFixed(2)}°');
+  print('$nilaiAsal° = ${suhuHasil[typeResult-1].toStringAsFixed(2)}°');
 }
